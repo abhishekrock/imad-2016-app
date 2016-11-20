2a9af3ef8fb5b64ba4570dae712e076f16ab84a5
abhishekrock <abhishekhit63@gmail.com>
2016-11-20 23:23:23 +0530

    [imad-console] Updates ui/main1.js

--- ui/main1.js
+++ ui/main1.js
@@ -1,4 +1,4 @@
-var submit = document.getElementById('register_btn');
+var submit = document.getElementById('login_btn');
     submit.onclick = function () {
         // Create a request object
         var request = new XMLHttpRequest();
@@ -39,7 +39,7 @@ var submit = document.getElementById('register_btn');
 //register start here
 
 
-var register = document.getElementById('register_btn1');
+var register = document.getElementById('register_btn');
     register.onclick = function () {
         // Create a request object
         var request = new XMLHttpRequest();

11348c86d1d909ebc81461a42a9fbc3cca9f8f2e
abhishekrock <abhishekhit63@gmail.com>
2016-11-21 00:05:18 +0530

    [imad-console] Updates ui/main2.js

--- ui/main2.js
+++ ui/main2.js
@@ -31,7 +31,7 @@ var register = document.getElementById('register_btn');
         console.log(password);
         request.open('POST', '/create-user', true);
         request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify({username: username, password: password}));  
+        request.send(JSON.stringify({username: username, password: password , name:name, email:email}));  
         register.value = 'Registering...';
     
     };

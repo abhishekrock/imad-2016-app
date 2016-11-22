4c6b55716bd41806e847275ece944cf7e4a92e98
abhishekrock <abhishekhit63@gmail.com>
2016-11-22 20:37:24 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -105,7 +105,7 @@ function loadComments () {
 				                    
 				                  <hr>
 				
-    	               				<p >	<b class="username"> ${commentsData[i].username} </b>${time.toLocaleTimeString()} on ${time.toLocaleDateString()} said:<p>
+    	               				<p >	<b class="username"> ${commentsData[i].username} </b>${time.toLocaleTimeString()} on ${time.toLocaleDateString()} said:<p> time.getMonth()
 
                                     ${escapeHTML(commentsData[i].comment)}
                                     
